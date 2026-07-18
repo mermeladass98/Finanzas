fba24639a59e835df022db714a56a5c758798d84
mermeladass98 <andresaa98@gmail.com>
2026-07-18 23:27:05 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "finanzas-v1";
+const CACHE = "finanzas-v2";
 const FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(FILES)).then(() => self.skipWaiting()));
@@ -7,7 +7,15 @@ self.addEventListener("activate", (e) => {
   e.waitUntil(caches.keys().then((ks) => Promise.all(ks.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim()));
 });
 self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request, { ignoreSearch: true }).then((r) => r || fetch(e.request).catch(() => caches.match("./index.html")))
-  );
+  if (e.request.mode === "navigate") {
+    e.respondWith(
+      fetch(e.request).then((r) => {
+        const copia = r.clone();
+        caches.open(CACHE).then((c) => c.put("./index.html", copia));
+        return r;
+      }).catch(() => caches.match("./index.html"))
+    );
+  } else {
+    e.respondWith(caches.match(e.request, { ignoreSearch: true }).then((r) => r || fetch(e.request)));
+  }
 });
